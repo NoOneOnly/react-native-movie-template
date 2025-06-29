f54e28d15dcf6bf735533d1da8d63a0336d33c29
Afaf <afafsyauqi@gmail.com>
2025-06-29 11:53:18 +0700

    chore: update dependencies and add Tailwind CSS configuration
    - Added `expo-dev-client` and `nativewind` to dependencies in package.json.
    - Updated `react-native-safe-area-context` version to use caret (^) for flexibility.
    - Removed the `reset-project.js` script as it is no longer needed.
    - Created a new `tailwind.config.js` file for Tailwind CSS configuration.
    - Updated `tsconfig.json` to include `nativewind-env.d.ts`.
    - Added type declarations for PNG images in `types/images.d.ts`.

---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const MoviesDetail = () => {
+  return (
+    <View>
+      <Text>MoviesDetail</Text>
+    </View>
+  );
+};
+
+export default MoviesDetail;
